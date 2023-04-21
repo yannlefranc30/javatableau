fce5d228faa87e423fde975533f2725782e0c78d
yann <yannlefranc330@gmail.com>
2023-04-21 11:57:32 +0200

    Java boucleforeach et tableau

---
+++ src/thewitcher.java
@@ -0,0 +1,17 @@
+public class thewitcher {
+
+    public static void main( String [] args ) {
+
+        // voici un tableau en java
+
+        int [] intthewitcher = { 20, 30, 60, 70, 80 };
+
+        //utilisation de la boucle for each
+
+        for( int value : intthewitcher ) {
+            System.out.println( value );
+        }
+
+    }
+
+}
